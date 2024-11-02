1dcfadf7e647cc6a6e72917df9accd765d02d19c
Sirin <sirin@Sirins-MacBook-Pro.local>
2024-11-02 21:23:35 +0100

    change repo name

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,3 @@
 module.exports = {
-  publicPath:
-    process.env.NODE_ENV === 'production' ? '/personal-website/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/portfolio/' : '/',
 }
