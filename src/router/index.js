23b08ca05c0444f48eaed8d5335ddfbb2abb48d4
Sirin <sirin@Sirins-MacBook-Pro.local>
2024-11-03 12:15:25 +0100

    test

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 console.log(import.meta.env.BASE_URL)
 
 const router = createRouter({
-  history: createWebHistory('/portfolio/'),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
