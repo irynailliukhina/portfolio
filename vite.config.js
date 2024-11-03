1f8fe7e277cdadc44dc9a30aaa153dbdaa110b56
Sirin <sirin@Sirins-MacBook-Pro.local>
2024-11-03 13:26:52 +0100

    test

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,14 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        fallback: 'index.html', // Ensure index.html serves as fallback
+      },
+    },
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
