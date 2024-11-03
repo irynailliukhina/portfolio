a274c25977aa3f36347321491cb69feb0a8f9759
Sirin <sirin@Sirins-MacBook-Pro.local>
2024-11-03 02:36:59 +0100

    setting gh

--- src/router/index.js
+++ src/router/index.js
@@ -2,14 +2,14 @@ import { createRouter, createWebHistory } from 'vue-router'
 console.log(import.meta.env.BASE_URL)
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory('/portfolio'),
   routes: [
     {
       path: '/',
       component: () => import('../views/HomeView.vue'),
     },
     {
-      path: '/work',
+      path: 'portfolio/work',
       component: () => import('../views/WorkView.vue'),
     },
     {
