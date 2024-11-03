8853028ecdf555b13d657331e650f1ceeac8b44c
Sirin <sirin@Sirins-MacBook-Pro.local>
2024-11-03 02:51:12 +0100

    setting gh

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,6 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/portfolio/' : '/',
+  publicPath:
+    process.env.NODE_ENV === 'production'
+      ? 'https://irynailliukhina.github.io/portfolio'
+      : '/',
 }
